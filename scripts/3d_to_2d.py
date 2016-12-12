3f84861894d3ba86afebfe314ee229da74ef8216
John Clarke <jaclarke@mit.edu>
2016-12-11 19:59:48 -0500

    3d to 2d

---
+++ scripts/3d_to_2d.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+X_3d_lattices = np.load('../data/datasets/resampled_X_lattices.npy')
+X_3d_shape = X_3d_lattices.shape
+n,X,Y,Z = X_3d_shape
+assert X == Y == Z
+
+X_2d_xy_transform = np.zeros((n,X,Y,1))
+X_2d_yz_transform = np.zeros((n,Y,Z,1))
+X_2d_xz_transform = np.zeros((n,X,Z,1))
+
+# perform xy transformation
+for num in range(n):
+	for i in range(X):
+		for j in range(Y):
+			val = 0
+			for count in range(1,Z): 
+				val = X_3d_lattices[num][i][j][Z-count]
+				if val != 0:
+					break
+			X_2d_xy_transform[num][i][j][0] = val
+
+# perform yz transformation
+for num in range(n):
+	for i in range(Y):
+		for j in range(Z):
+			val = 0
+			for count in range(1,X): 
+				val = X_3d_lattices[num][i][j][X-count]
+				if val != 0:
+					break
+			X_2d_yz_transform[num][i][j][0] = val
+
+# perform xz transformation
+for num in range(n):
+	for i in range(X):
+		for j in range(Z):
+			val = 0
+			for count in range(1,Y):
+				val = X_3d_lattices[num][i][j][Y-count]
+				if val != 0:
+					break
+			X_2d_xz_transform[num][i][j][0] = val
+
+print X_2d_xy_transform.shape
+
+
+
+
+
+
+
+
+
